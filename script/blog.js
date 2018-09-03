2acf018008c7069785e8c995b6368f81f7bc0762
2mylesaway <myles.crouther@gmail.com>
2018-09-02 20:23:43 -0400

    Cosmetics & Blog Start

---
+++ script/blog.js
@@ -0,0 +1,60 @@
+var tile = document.getElementsByClassName("blog")[0];
+var items = json.items;
+var maxLength = (items.length) / 4;
+
+for( var i = 0; i < items.length; i++) {
+  /**
+    
+    if (i < maxLength) {var tile = start;}
+    if ((maxLength <= i) && (i < maxLength*2)) {var tile = document.getElementsByClassName("blogc2")[0];}
+    if ((maxLength*2 <= i) && (i < maxLength*3)) {var tile = document.getElementsByClassName("blogc3")[0];}
+    if ((maxLength*3 <= i) && (i < maxLength*4)) {var tile = document.getElementsByClassName("blogc4")[0];}
+  **/
+
+	var div = document.createElement("div");
+	div.classList.add("tileBlock");
+	tile.appendChild(div);
+
+  if(items[i].url != null){
+    var anchor = document.createElement("a");
+    anchor.classList.add("tileImg");
+    anchor.href = items[i].url;
+  }
+
+  else{
+    var anchor = document.createElement("a");
+    anchor.classList.add("tileImg");
+    anchor.href = "#";
+  }
+
+  if(items[i].img != null){
+    var img = document.createElement("img");
+    img.setAttribute("src", items[i].img);
+
+    if(items[i].icon != null){
+      var inDiv = document.createElement("div");
+      inDiv.classList.add("tbIcon");
+
+      if(items[i].iconBkgc != null){
+        inDiv.classList.add(items[i].iconBkgc);
+      }
+      inDiv.appendChild(img);
+      anchor.appendChild(inDiv);
+    }
+    else{anchor.appendChild(img);}
+  }
+    
+    var text = document.createElement("div");
+    text.classList.add(items[i].color);
+
+    var h3 = document.createElement("h3");
+    h3.innerHTML = items[i].header;
+    text.appendChild(h3);
+
+    var p = document.createElement("p");
+    p.innerHTML = items[i].description;
+    text.appendChild(p);
+
+    anchor.appendChild(text);
+    div.appendChild(anchor);
+}
---
+++ script/data.js
@@ -0,0 +1,245 @@
+/** data.json **/
+
+var json = {"items": [
+  {
+     "header": "About",
+     "description": "Avid developer, designer and maker...",
+     "color": "white",
+     "img":"../img/bkg/20161209.jpeg",
+     "url":"https://crouther.github.io"
+   },
+
+  {
+    "header": "Work",
+    "description": "My time in the industry",
+    "color": "lightgray",
+    "img": "../img/bkg/wrktbl.jpg",
+    "url":"https://crouther.github.io"
+   },
+
+   {
+    "header": "Research",
+    "description": "Fact-finding analysis from academic studies",
+    "color": "tan",
+    "img":"../img/rsch.jpg",
+    "url":"https://crouther.github.io/p/research"
+   },
+
+    {
+    "header": "Art",
+    "description": "Inspirational creative releases to keep the mind busy",
+    "color": "yellow",
+    "img":"../img/bkg/sc.png",
+    "url":"https://crouther.github.io/p/art"
+   },
+
+  {
+    "header": "Design",
+    "description": "Designer's visions turned into reality. Mentored Professionally.",
+    "color": "white",
+    "img": "../img/icon/graphic-design.svg",
+    "icon": "yes",
+    "iconBkgc": "tomato",
+    "url":"https://crouther.github.io/p/design"
+   },
+
+  {
+    "header": "Hardware",
+    "description": "IT consulting, contracting, and side projects.",
+    "color": "white",
+    "img":"../img/icon/hardware.svg",
+    "icon":"yes",
+    "iconBkgc": "limegreen",
+    "url":"https://crouther.github.io/p/hardware"
+   },
+
+   {
+    "header": "Software",
+    "description": "Freelance development, contracting, and side projects.",
+    "color": "white",
+    "img":"../img/icon/coding.svg",
+    "icon":"yes",
+    "iconBkgc": "lightblue",
+    "url":"https://crouther.github.io/p/software"
+   },
+
+  {
+    "header": "Hewlett Packard Enterprise",
+    "description": "Work in their Enterprise Security Solutions and Strategic Growth & Innovation.",
+    "color": "hpe",
+    "img":"../img/icon/hpe.png",
+    "icon":"yes",
+    "iconBkgc": "darkGray",
+    "url":"https://crouther.github.io/p/hpe"
+   },
+
+  {
+    "header": "Hewlett Packard",
+    "description": "Work in Strategic Growth & Innovation and Emerging Technologies Devision",
+    "color": "hp",
+    "img":"../img/icon/hp.png",
+    "icon":"yes",
+    "iconBkgc": "white",
+    "url":"https://crouther.github.io/p/hp"
+   },
+
+  {
+    "header": "Emagine IT",
+    "description": "A small business sub contractor for HP. Part of their Mentor Protégé  Program.",
+    "color": "white",
+    "img":"../img/icon/eit.png",
+    "icon":"yes",
+    "iconBkgc": "eit",
+    "url":"https://crouther.github.io/p/eit"
+   },
+
+  {
+    "header": "Web Design",
+    "description": "Custom templates developed in excess from building a design, software and hardware portfolio to showcase my industry and personal projects since 2013.",
+    "color": "white",
+    "img":"../img/icon/web-design.svg",
+    "icon":"yes",
+    "iconBkgc": "tomato",
+    "url":"https://crouther.github.io/p/web"
+   },
+
+  {
+    "header": "Wireframes",
+    "description": "Custom wireframe designs from previous employer contracts, projects and concept designs.",
+    "color": "white",
+    "img":"../img/icon/logo.png",
+    "icon":"yes",
+    "iconBkgc": "lightgray",
+    "url":"https://crouther.github.io/p/wireframes"
+   },
+
+  {
+    "header": "Cypher",
+    "description": "So you think you can rap?",
+    "color": "white",
+    "img":"../img/icon/cypher.png",
+    "icon":"yes",
+    "iconBkgc": "yellow",
+    "url":"https://crouther.github.io/p/cypher"
+
+   },
+
+  {
+    "header": "Just Cards",
+    "description": "A deck you can touch, cards you can share.",
+    "color": "dimgray",
+    "img":"../img/icon/jc.png",
+    "url":"https://crouther.github.io/p/justCards"
+   },
+
+  {
+    "header": "Otterbourg",
+    "description": "Concept web design for a law firm",
+    "color": "lightblue",
+    "url":"https://crouther.github.io/p/otterbourg"
+   },
+
+  {
+    "header": "Apple",
+    "description": "Summary of my IT experience with consumer electronics at Apple's Reston destination location.",
+    "color": "white",
+    "img":"../img/icon/apple-logo.png",
+    "icon":"yes",
+    "iconBkgc": "lightgray",
+    "url":"https://crouther.github.io/p/apple"
+   },
+
+  {
+    "header": "Server",
+    "description": "Restoration of a Google Search Appliance for alternative means.",
+    "color": "white",
+    "img":"../img/icon/data.svg",
+    "icon":"yes",
+    "iconBkgc": "yellow",
+    "url":"https://crouther.github.io/p/server"
+   },
+
+  {
+    "header": "Custom Workstation",
+    "description": "Assembled with care.",
+    "color": "white",
+    "img":"../img/icon/processor.svg",
+    "icon":"yes",
+    "iconBkgc": "tan",
+    "url":"https://crouther.github.io/p/workstation"
+   },
+
+  {
+    "header": "Ducati",
+    "description": "Photos, videos and tutorials of the maintenance and work I've done on my motorcycle.",
+    "color": "white",
+    "img":"../img/icon/ducati.png",
+    "icon":"yes",
+    "iconBkgc": "ducati",
+    "url":""
+   },
+
+  {
+    "header": "Wilderness Workbench",
+    "description": "A new table for conversation. Guiding the compass needle one step further.",
+    "color": "white",
+    "img":"../img/icon/workbench.svg",
+    "icon":"yes",
+    "iconBkgc": "limegreen",
+    "url":"https://crouther.github.io/p/table"
+   },
+
+  /**
+    {
+      "header": "The Internet of Things",
+      "description": "",
+      "color": "tan"
+     },
+
+     {
+      "header": "Cryptography",
+      "description": "",
+      "color": "tan"
+     },
+
+    {
+      "header": "Quantum Computing",
+      "description": "",
+      "color": "tan"
+     },
+
+    {
+      "header": "Digital Privacy",
+      "description": "",
+      "color": "tan"
+     },
+
+    {
+      "header": "Big Data",
+      "description": "",
+      "color": "tan"
+     },
+
+    {
+      "header": "Artificial Intelligence",
+      "description": "",
+      "color": "tan"
+     },
+  **/
+
+  {
+    "header": "Book",
+    "description": "Take A Trip, a pending novel by the works of Myles Crouther",
+    "color": "tan",
+    "img":"../img/bkg/book169.jpg",
+    "url":"https://crouther.github.io/p/book"
+   },
+
+  {
+    "header": "Photography",
+    "description": "My journey through south east asia.",
+    "color": "white",
+    "img":"../img/bkg/koreaPan.jpg",
+    "url":"https://crouther.github.io/p/pictures"
+   },
+]};
