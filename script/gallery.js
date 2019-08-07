f4aa57b61da04fa869bcf754e951b746ab6e93cc
myles <inbox@myles.works>
2019-08-06 23:06:25 -0400

    Gallery Update

--- script/gallery.js
+++ script/gallery.js
@@ -8,50 +8,6 @@
 **/
 
 
-//Find Gallery By Hash Tag
-var pathname = document.location.href.toString();
-var nation = pathname.substring(pathname.length-2, pathname.length-1);
-var town = pathname.substring(pathname.length-1, pathname.length);
-
-//Title and Place Named
-document.getElementById("JourneyName").innerHTML = gallery.Country[nation].name;
-document.getElementById("CityName").innerHTML = gallery.Country[nation].city[town].name;
-
-//Image Container Created
-var mwBox = document.createElement("div");
-mwBox.setAttribute("class", "mw-box");
-
-//Primary Image Displayed
-var img1 = new Image();
-img1.src = gallery.Country[nation].city[town].images[0];
-img1.setAttribute("class", "headerImg");
-
-mwBox.appendChild(img1); 
-
-//Secondary Images Container
-var fb4c = document.createElement("div");
-fb4c.setAttribute("class", "fb4c");
-
-//Secondary Images Population Loop
-for (i = 1; i < gallery.Country[nation].city[town].images.length; i++) {
-
-	var embeddedItem = document.createElement("div");
-	embeddedItem.setAttribute("class", "embeddedItem");
-
-	var tempIMG = new Image();
-	tempIMG.src = gallery.Country[nation].city[town].images[i];
-	tempIMG.setAttribute("class", "MKCBars loadingAnimation");
-
-	embeddedItem.appendChild(tempIMG);
-	fb4c.appendChild(embeddedItem); 
-}
-
-//Update Interface to Display Image Gallery
-mwBox.appendChild(fb4c);
-document.getElementById("img-container").appendChild(mwBox);
-
-/** New Content Section **/
-
 //Places Container Created
 var placesmwBox = document.createElement("div");
 placesmwBox.setAttribute("class", "mw-box");
@@ -108,3 +64,47 @@ elementsArray.forEach(function(elem) {
         location.reload();
     });
 });
+
+/** New Content Section **/
+
+//Find Gallery By Hash Tag
+var pathname = document.location.href.toString();
+var nation = pathname.substring(pathname.length-2, pathname.length-1);
+var town = pathname.substring(pathname.length-1, pathname.length);
+
+//Title and Place Named
+document.getElementById("JourneyName").innerHTML = gallery.Country[nation].name;
+document.getElementById("CityName").innerHTML = gallery.Country[nation].city[town].name;
+
+//Image Container Created
+var mwBox = document.createElement("div");
+mwBox.setAttribute("class", "mw-box");
+
+//Primary Image Displayed
+var img1 = new Image();
+img1.src = gallery.Country[nation].city[town].images[0];
+img1.setAttribute("class", "headerImg");
+
+mwBox.appendChild(img1); 
+
+//Secondary Images Container
+var fb4c = document.createElement("div");
+fb4c.setAttribute("class", "fb4c");
+
+//Secondary Images Population Loop
+for (i = 1; i < gallery.Country[nation].city[town].images.length; i++) {
+
+	var embeddedItem = document.createElement("div");
+	embeddedItem.setAttribute("class", "embeddedItem");
+
+	var tempIMG = new Image();
+	tempIMG.src = gallery.Country[nation].city[town].images[i];
+	tempIMG.setAttribute("class", "MKCBars loadingAnimation");
+
+	embeddedItem.appendChild(tempIMG);
+	fb4c.appendChild(embeddedItem); 
+}
+
+//Update Interface to Display Image Gallery
+mwBox.appendChild(fb4c);
+document.getElementById("img-container").appendChild(mwBox);
