00475066c8011f33a738dfdcd2d5d84067715250
myles <inbox@myles.works>
2019-11-05 17:54:36 -0500

    Timeline Update
    Styling, content, and documentation.
    
    Timeline.js : A reusable, documentated and modular web component

--- script/timeline.js
+++ script/timeline.js
@@ -12,38 +12,109 @@ function Timeline(event, target){
 
 	for(t = 0; t < event.milestone.length; t++){
 
-		//console.log(t);
+		//Milestone Variable Components
 		var container = document.createElement("div");
-
-		if( t > 0 && t % 2 == 1){
-			container.setAttribute("class", "container right");
-		}
-		else{container.setAttribute("class", "container left");}
-
 		var content = document.createElement("div");
-		content.setAttribute("class", "content");
 
-		var h2 = document.createElement("h2");
+		var h3 = document.createElement("h3");
 		var p = document.createElement("p");
 
-		h2.innerHTML = event.milestone[t].date;
+		//Milestone Variable Styles
+		content.setAttribute("class", "content");
+
+		if( t > 0 && t % 2 == 1)	// if odd (position right)
+			container.setAttribute("class", "container right");
+		else
+			container.setAttribute("class", "container left");
+		
+		h3.innerHTML = event.milestone[t].date;
 		p.innerHTML = event.milestone[t].brief;
 
-		content.appendChild(h2);
+		content.appendChild(h3);
 
+		//Builds Anchor if a link is given for waypoint
 		if(event.milestone[t].link != null){
 
 			var a = document.createElement("a");
-			a.setAttribute("href", ( event.milestone[t].link.toString() ) );
+			a.setAttribute("href", (event.milestone[t].link.toString()) );
+
 			a.appendChild(p);
 			content.appendChild(a);
 		
 		}
-
-		else{content.appendChild(p);}
+		else{ content.appendChild(p); }
 
 		container.appendChild(content);
-
-		document.getElementById( ( target.toString() ) ).appendChild(container);
+		document.getElementById( (target.toString()) ).appendChild(container);
 	}
 }
+
+
+
+
+/*******************************  DOCUMENTATION  *******************************
+
+
+Timeline(event, target)
+
+
+Parameters -
+
+event:
+	The json object or JavaScript array imported, parsed or passed possessing
+	timeline milestones and details regarding each waypoint.
+
+target:
+	The id tag name for a div found in the html source file for appending each
+	additional waypoint and detail box to the timeline
+
+	<div class="timeline" id = "target">
+
+
+Function Output -
+
+	The target div appends child elements which resemble the code -
+
+	  <div class="container left">
+	    <div class="content">
+	      <h2>2017</h2>
+	      <p>Lorem ipsum..</p>
+	    </div>
+	  </div>
+
+	  or
+
+	  <div class="container right">
+	    <div class="content">
+	      <h2>2016</h2>
+	      <p>Lorem ipsum..</p>
+	    </div>
+	  </div>
+
+
+Pre-requisites -
+
+	Include the following scripts and files (with modified paths) and function 
+	calls to activate the Timeline component.
+
+
+	At the beginning of your HTML file:
+
+	<link rel="stylesheet" type="text/css" href="../w3-timeline.css">
+
+	<script src="../YOURDATA.js"></script>
+
+	At the end of your HTML file:
+
+	<script src="../timeline.js"></script>
+	<script type="text/javascript">Timeline(YOURJSON,"w3-timeline")</script>
+
+
+Tips -
+
+	To avoid styling overlap, it is best to included the timeline component 
+	in an encapsulated iFrame. This will allow the CSS' universal styling to be 
+	uninterrupted. or simply remove/transplant the * and body css properties.
+
+
+*******************************************************************************/
