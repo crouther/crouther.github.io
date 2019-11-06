553afc128d58d41bd7460f3f05e3078d8a82f714
myles <inbox@myles.works>
2019-11-06 15:02:40 -0500

    Update motov-time.js

--- script/motov-time.js
+++ script/motov-time.js
@@ -9,17 +9,17 @@ var motovTimeline = {
 		{
 			"date" : "July 2019",
 			"brief" : "Prototype: <br><br> Animations, creative commons outsourcing and unorthodox design partnerships to reach a more formalized look at the user experience.",
-			"link" : "https://myles.works/on/motocluster",
+			"link" : "https://myles.works/on/motocluster#DesignDetails",
 		},
 		{
 			"date" : "August 2019",
 			"brief" : "MVP & User Group Testing: <br><br> Initial audience overlook and discussion of core functionality and completition of the minimum viable product. Focus group sized at approximately 14 people.",
-			"link" : "https://myles.works/on/motocluster",
+			"link" : "https://myles.works/on/motocluster#08262019",
 		},
 		{
 			"date" : "September 2019",
 			"brief" : "Marketing Materials: <br><br> Promotional material and marketing and advertising at conferences and entrepenuership events to gauge market interest and investor relations.",
-			"link" : "https://myles.works/on/motocluster",
+			"link" : "https://motov.app",
 		},
 		{
 			"date" : "October - November 2019",
@@ -29,17 +29,17 @@ var motovTimeline = {
 		{
 			"date" : "December - January 2020",
 			"brief" : "Peripherals: <br><br> designing the neccessary support hardware to reverse engineer universal locomotive support for the application.",
-			"link" : "https://myles.works/on/motocluster",
+			"link" : "https://myles.works/on/motocluster#mvHDWE",
 		},
 		{
 			"date" : "Spring 2020",
-			"brief" : "Grand Release",
-			"link" : "https://myles.works/on/motocluster",
+			"brief" : "<a href=\"https://motov.app\"><img style =\"width: 100%; height: auto; padding: 5%;\" src=\"https://myles.works/img/road.svg\"></a> Grand Release",
+			"link" : "https://motov.app",
 		},
 		{
 			"date" : "Summer 2020",
-			"brief" : "Post Launch Update (AI Tiers of Influence)",
-			"link" : "https://myles.works/on/motocluster",
+			"brief" : "Post Launch Update (AI Tiers of Influence): <br><ul><li>Core mvp (w/ engine stethoscope)</li><li>Moto cluster removal kit</li><li>Auto.motov (w/ obd reader)</li><li>Awareness System</li><li>Assisted driver</li><li>Electric Motorcycle</li></ul>",
+			"link" : "https://motov.app",
 		}
 	]
 }
