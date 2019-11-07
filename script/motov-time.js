2bc7ff07c7ccfb143ad3ca19fb6d284739fa4435
myles <inbox@myles.works>
2019-11-07 14:38:34 -0500

    Content & Path Updates

--- script/motov-time.js
+++ script/motov-time.js
@@ -3,12 +3,12 @@ var motovTimeline = {
 	"milestone": [
 		{
 			"date" : "May - June 2019",
-			"brief" : "Concept Art: <br><br> Wireframes to visualize the user experience of the application. Feature layouts, core components and various additional insights to the overall look and feel.",
+			"brief" : "Concept Art: <br><a href=\"https://myles.works/on/motocluster\"><img style =\"width: 100%; height: auto; padding: 5%;\" src=\"https://lh3.googleusercontent.com/iQb-pexYHCZ6sQ55u6SlBgbBNgA3zGNQM9yoDXzgAEHwYUIXp-COea2jTsHdVrV7O3VfvSDeKEkpuie1KgXi3DS6_WW5lu12aA8tsLq0VT8qvL1igcU4PS5dvjOJM7lwpvegRg6wlQK8FVQypmWHAVPYMhjFuzmzcpsbyEKUu_c1tOfrzmU5S3pQe6vuRVoSzjmeJ7D3lx7qWCuTNzTL60I1Az2rs8maeILg7K-UeZQ3ScAsQ_8ZaYJOYek2AmzRd37Udmff4WOox0LS20eZ9M7dxep6s12XRPauyMOGDLr2nVnGLwgoNaj56Vm5VOP36P1v_x9HJh0qrsyJNIBlzvLbeLougtK2n-a4GQrQnr0_uADLAfTSX6-ZiTQSB_sa-v5_rBxK15cbZvGZ8QDkrl-a4_cetfUmO-DJZS0VXsTJnDxPgXhjpdH3lgUq36Sd7sQlvMGRUqOYuyAzBmrksCtLSr_JCIZUxuFPV89OyCYAuwf5znZbd8ho215IMFDyZmFPoDzw4n9PMQ8sszG_I-ndN7lsLNasZwowdPjKwEVhvjLJau9yC6PtIvKsGQ-fCQMHdJ5aArKKZQ-vNul8ivFrh8OdlJccqYskdlU-FUSuBF9ajVmPOpgRxgA4A9saRqEq2neM264zRCuzfyYDOWwE_McWl4ryjmT_Ufys26GsI-t54s2kJaufFJwT5qa-zBivhYBiKUn2qMiJazOCC_KW_GuM5POBpFL6MjcDI_LahE40=w2268-h1270-no\"></a><br> Wireframes, sketches, and flat graphics to visualize the user experience of the application. Feature layouts, core components and various additional insights to the overall look and feel.",
 			"link" : "https://myles.works/on/motocluster",
 		},
 		{
 			"date" : "July 2019",
-			"brief" : "Prototype: <br><br> Animations, creative commons outsourcing and unorthodox design partnerships to reach a more formalized look at the user experience.",
+			"brief" : "Prototype: <br><a href=\"https://myles.works/on/motocluster\"><img style =\"width: 100%; height: auto; padding: 5%;\" src=\"https://lh3.googleusercontent.com/bKkkqMFYKx-MIBSrATF9Hu6Non4M6J76AGjM0p-OVZ4MM20w_sf7h0hgSgZU-6vvRxphiY6lWO7vbtPuCRWyfmM84Va4Ql3s4p6jy0cPE6fvhytwPNT-MYzLVKe7z87M_ou-oWq9fqDfAvQb7YyjBY5JiXu2t1ywYU1mw5tJhKCXzVFG8572KBFMHqD5sFKMnDAH02m4NxPTxAs20pz-H905-nlis6gcHbKIb-5yZR-CDYlA47y9VRGMc1FYzQje7lYwOJ1QI_JvuDduzsRphlySJCyYeG_H7OAyXFXwMoi-C540mRnZStb7DoSibdkJnwrm_GwhDFmwFnKz-TrK4pGOl87YAaWQrsXNFkqFFiJdUjnocTesaNFpszUMinVj017gTlapwDBhBYxEhWwVcpHnLf4n6ddZMcR1PgmPuxbIdQFR0ARezhDLs968BASl-a4i392Ho7QM0SgGIWLJbfNCaDDvMRCkxe7wxmTlEZwQLTecPclKyziZhuTEG8cT-e8FJYLfYqaO3r_vWDEP3AsHXjm05UDZYgFGa22sM6m5TCJnvw9fZkRuA1BziT50AEJKGcyyTFSYfRw7a7PodM05etcikfa67G-o5kqtbTXYMC37g2-mWhtYcFxPAxURY8rdCLxaA3u3mNniKwgDhWWMQ2o5vmoZSe9bv0l_3ax71FYKKc8w43dqWKP23XolBQmFY8kyZVsz1G9Q-BKbuSevTgANfUgOIqdlFLn0Qvfpopug=w1920-h1006-no\"></a><br> Animations, creative commons outsourcing and unorthodox design partnerships to reach a more formalized look at the user experience.",
 			"link" : "https://myles.works/on/motocluster#DesignDetails",
 		},
 		{
